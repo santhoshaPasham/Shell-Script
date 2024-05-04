42194ce27774489ce6baabb57c779689dbfe5e77
pasham7 <santhoshapasham728@gmail.com>
2024-05-05 01:19:49 +0530

    this function

--- 11-functions.sh
+++ 11-functions.sh
@@ -5,7 +5,7 @@ TIMESTAMP=$(date +%F-%H-%S)
 SCRIPTNAME=$( echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
 R="\e[31m"
-G ="\e[32m"
+G="\e[32m"
 N="\e[0m"
 
 if [ $USERID -ne 0 ]
@@ -20,7 +20,7 @@ fi
 VALIDATE(){
 if [ $1 -ne 0 ]
 then 
-    echo  -e "$2 is $R failure $N"
+    echo -e "$2 is $R failure $N"
     exit 1
 else 
     echo -e "$2 is $G success $N"
