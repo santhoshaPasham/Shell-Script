6ef84a6ba4dc330e2ae99787c0f0bdbb631835d0
pasham7 <santhoshapasham728@gmail.com>
2024-05-04 14:43:29 +0530

    this is the second  commit

--- 10-installation.sh
+++ 10-installation.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 
-if [$USERID -ne 0]
+if [ $USERID -ne 0 ]
 then 
     echo "Please run this command with rot access"
     exit 1
@@ -12,7 +12,7 @@ fi
 
 dnf install mysql -y 
 
-if [$? -ne 0]
+if [ $? -ne 0 ]
 then 
     echo "Mysql installation is failure"
     exit 1
@@ -22,7 +22,7 @@ fi
 
 dnf install git -y 
 
-if [$? -ne 0]
+if [ $? -ne 0 ]
 then 
     echo "Git installation is failure"
     exit 1
