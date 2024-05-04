2fcfaf134440127b7dd204e44461964b8dfda440
pasham7 <santhoshapasham728@gmail.com>
2024-05-04 14:39:18 +0530

    this is the second  commit

---
+++ 10-installation.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [$USERID -ne 0]
+then 
+    echo "Please run this command with rot access"
+    exit 1
+else
+    echo "you are super user"
+fi 
+
+dnf install mysql -y 
+
+if [$? -ne 0]
+then 
+    echo "Mysql installation is failure"
+    exit 1
+else 
+    echo "Installation of mysql is success"
+fi 
+
+dnf install git -y 
+
+if [$? -ne 0]
+then 
+    echo "Git installation is failure"
+    exit 1
+
+else 
+    echo "Git installation is success"
+fi
+
+echo "is script proceeding" 
