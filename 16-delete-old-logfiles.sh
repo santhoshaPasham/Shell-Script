0caab36c774c9350fec90025091565cb26b2a7ed
pasham7 <santhoshapasham728@gmail.com>
2024-05-07 23:11:51 +0530

    Set common

--- 16-delete-old-logfiles.sh
+++ 16-delete-old-logfiles.sh
@@ -8,8 +8,16 @@ then
     echo -e "$G Folder exists $N"
 else
     echo -e "$R please make sure $SOURCE_DIRECTORY exists $N"
+    exit 1
 fi
 
+FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
 
+echo "Files to delete:$FILES"
 
+while IFS=read -r line 
+do 
+    echo "Deleting file: $line"
+    rm -rf $line
+done <<< $FILES
 
