d9b95313c5e958f390bff6e28f126a893459ccfc
pasham7 <santhoshapasham728@gmail.com>
2024-05-09 01:07:37 +0530

    Disk Usage

--- 17-Disk-usage.sh
+++ 17-Disk-usage.sh
@@ -3,7 +3,7 @@ THRESHOLD=6
 
 while IFS= read -r line 
 do 
-    USAGE=$(echo $line | awk -F " " '{print $6F})' | cut -d "%" -f1)
+    USAGE=$(echo $line | awk -F " " '{print $6F}') | cut -d "%" -f1)
     FOLDER=$(echo $line | awk -F " " '{print $NF}')
     if [ $USAGE -ge $THRESHOLD ]
     then 
